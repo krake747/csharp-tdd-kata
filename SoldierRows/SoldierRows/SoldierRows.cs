b56b229d91dcad5a0ab885b1fa07d11b5da22963
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-09-08 21:34:50 +0200

    ✏️ type: Rename expression.

--- SoldierRows/SoldierRows/SoldierRows.cs
+++ SoldierRows/SoldierRows/SoldierRows.cs
@@ -4,7 +4,7 @@ public class SoldierRows
 {
     public int[] KWeakestRows(int[][] mat, int k) => 
         Enumerable.Range(0, mat.GetLength(0))
-                  .Select(m => new { Row = m, Soldiers = mat[m].Count(i => i == 1) })
+                  .Select(r => new { Row = r, Soldiers = mat[r].Count(i => i == 1) })
                   .OrderBy(r => r.Soldiers)
                   .ThenBy(r => r.Row)
                   .Select(r => r.Row)
