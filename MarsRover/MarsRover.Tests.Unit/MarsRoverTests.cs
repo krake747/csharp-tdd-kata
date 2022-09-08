c0e8be97c508e9b70f2d79556f0c511fd9473c2e
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-09-08 19:06:44 +0200

    ⬆️ build(deps): Update dependencies.

---
+++ MarsRover/MarsRover.Tests.Unit/MarsRoverTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using FluentAssertions;
+
+namespace MarsRover.Tests.Unit;
+
+public class MarsRoverTests
+{
+    [Fact]
+    public void Test1()
+    {
+        //var rover = new MarsRover([0, 0], Direction.East,[50,50]);
+
+    }
+}
---
+++ MarsRover/MarsRover/MarsRover.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MarsRover;
+
+public class MarsRover
+{
+    public int[,] Grid { get; }
+    public Direction Direction { get; private set; }
+    public int[,] Location { get; private set; }
+    
+    public MarsRover(int[,] location, Direction direction, int[,] grid)
+    {
+        Location = location;
+        Direction = direction;
+        Grid = grid;
+    }
+}
+
+public enum Direction
+{
+    North,
+    South,
+    East,
+    West,
+}
+
