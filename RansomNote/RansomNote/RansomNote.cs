632b3c752d5bc8244d2fa4c04cb13a3058b1a891
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-09-09 14:11:17 +0200

    🎉 Init(kata): RansomNote.

---
+++ RansomNote/RansomNote/RansomNote.cs
@@ -0,0 +1,16 @@
+﻿namespace RansomNote;
+
+public class RansomNote
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var groupNote = ransomNote.GroupBy(x => x)
+                                  .ToDictionary(g => g.Key, g => g.Count());
+
+        var groupMagazine = magazine.GroupBy(x => x)
+                                    .ToDictionary(g => g.Key, g => g.Count());
+
+        return groupNote.All(x => groupMagazine.ContainsKey(x.Key)
+                                  && groupMagazine[x.Key] >= x.Value);
+    }
+}
