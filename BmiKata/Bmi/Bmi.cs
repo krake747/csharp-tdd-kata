571bdd61f01e642b658d2383d0218a86a645acd5
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-09-12 15:35:26 +0200

    🎉 Init(kata): Bmi calculator.

---
+++ BmiKata/Bmi/Bmi.cs
@@ -0,0 +1,43 @@
+﻿namespace Bmi;
+
+public static class Bmi
+{
+    public static void Run()
+    {
+        Run(Read, Write);
+    }
+
+    internal static void Run(Func<string, double> read, Action<BmiRange> write)
+    {
+        double height = read("height");
+        double weight = read("weight"); 
+
+        var bmiRange = CalculateBmi(height, weight).ToBmiRange();
+
+        write(bmiRange);
+    }
+
+    internal static double CalculateBmi(double height, double weight) => Math.Round(weight / Math.Pow(height, 2), 2);
+
+    internal static BmiRange ToBmiRange(this double bmi) => bmi switch
+    {
+        < 18.5 => BmiRange.Underweight,
+        <= 25 => BmiRange.Healthy,
+        _ => BmiRange.OverWeight
+    };
+
+    private static double Read(string field)
+    {
+        Console.WriteLine($"Please enter your {field} (in m)");
+        return double.Parse(Console.ReadLine()!);
+    }
+
+    private static void Write(BmiRange bmiRange) => Console.WriteLine($"Based on your Bmi, you are {bmiRange}");
+}
+
+public enum BmiRange
+{
+    Underweight,
+    Healthy,
+    OverWeight
+}
---
+++ BmiKata/readme.md
@@ -0,0 +1,14 @@
+# BMI
+
+Write a console app that calculates a user’s Body Mass Index (BMI):
+
+1) Prompt the user for their `height` in meters and `weight` in kilograms.
+2) Calculate the BMI as `weight / height^2^`.
+3) Output a message: `underweight` (BMI < 18.5), `overweight` (BMI >= 25), or `healthy`.
+4) Structure your code so that pure and impure parts are separate.
+5) Unit test the pure parts.
+6) Unit test the overall workflow using the function-based approach to abstract away the reading from and writing to the console.
+
+# Source
+
+Chapter 3 - Functional Programming in C#: How to Write Better C# Code (2nd Edition)
